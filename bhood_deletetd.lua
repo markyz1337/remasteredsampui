ca54f99b27d0f0a84ae4124a19ecd0e8969022df
markyz1337 <112336168+markyz1337@users.noreply.github.com>
2022-12-07 12:02:30 +0000

    Update bhood_deletetd.lua

--- bhood_deletetd.lua
+++ bhood_deletetd.lua
@@ -1,5 +1,5 @@
 local sampev = require 'lib.samp.events'
-local tds = {172,173,174,2126,2127,2131} --айди текстдравов
+local tds = {178,179,180,2126,2127,2131} --айди текстдравов
 
 function sampev.onShowTextDraw(id, data)
     for i = 1, #tds do
