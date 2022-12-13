3aebc861a1a98a3ffb6b9ce8a465d6e2c51b0bba
markyz1337 <112336168+markyz1337@users.noreply.github.com>
2022-12-13 13:24:00 +0000

    Update nephrite_tdeditor.lua

--- nephrite_tdeditor.lua
+++ nephrite_tdeditor.lua
@@ -15,6 +15,75 @@ function se.onShowTextDraw(id, data)
 		data.position.y = data.position.y - 9
 	end
 	
+	if id == 129 then -- inventar boxul de jos
+        data.boxColor = 0xFF000000
+	end
+
+	if id == 130 then -- inventar veh accs misc etc
+		data.position.x = data.position.x - 432
+		data.position.y = data.position.y - 73
+		data.style = 3
+        data.boxColor = 0xFF000000
+	end
+	
+	if id == 131 then -- inventar veh accs misc etc
+		data.position.x = data.position.x - 432
+		data.position.y = data.position.y - 73
+		data.style = 3
+        data.boxColor = 0xFF000000
+	end
+	
+	if id == 132 then -- inventar veh accs misc etc
+		data.position.x = data.position.x - 432
+		data.position.y = data.position.y - 73
+		data.style = 3
+        data.boxColor = 0xFF000000
+	end
+	
+	if id == 133 then -- inventar veh accs misc etc
+		data.position.x = data.position.x - 432
+		data.position.y = data.position.y - 73
+		data.style = 3
+        data.boxColor = 0xFF000000
+	end
+	
+	if id == 134 then -- inventar veh accs misc etc
+		data.position.x = data.position.x - 432
+		data.position.y = data.position.y - 73
+		data.style = 3
+        data.boxColor = 0xFF000000
+	end
+	
+	if id == 2168 then -- inventory vehicles
+		data.style = 3
+        data.boxColor = 0xFF000000
+	end
+	
+	if id == 2170 then -- inventory numele jucatorului
+		data.style = 3
+        data.boxColor = 0xFF000000
+	end
+	
+	if id == 2174 then -- inventory hunger
+		data.style = 3
+	end
+	
+	if id == 2178 then -- inventory hunger
+		data.style = 3
+	end
+	
+	if id == 2177 then -- inventory thirst
+		data.style = 3
+	end
+	
+	if id == 2179 then -- inventory thirst
+		data.style = 3
+	end
+	
+	if id == 2180 then -- inventory level
+		data.style = 3
+	end
+	
 	if id == 2079 then -- payday text
 		data.position.x = data.position.x - 10
 		data.position.y = data.position.y - 9
