e631feeab66536a9044cec8898ca8ccabf3a14a6
markyz1337 <112336168+markyz1337@users.noreply.github.com>
2023-12-09 19:52:48 +0100

    Update INTERFACE_DELETETD_BHOOD.lua

--- INTERFACE_DELETETD_BHOOD.lua
+++ INTERFACE_DELETETD_BHOOD.lua
@@ -1,5 +1,5 @@
 local sampev = require 'lib.samp.events'
-local tds = {178,179,180,2126,2127,2131} --айди текстдравов
+local tds = {240,241,2114} --айди текстдравов
 
 function sampev.onShowTextDraw(id, data)
     for i = 1, #tds do
